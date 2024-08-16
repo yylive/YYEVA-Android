6a714de559b5b2c01febce23fee248b2b086b269
zengjiale2 <zengjiale2@yy.com>
2024-08-16 17:33:49 +0800

    修复混合问题

--- yyevac/src/main/cpp/engine/render.cpp
+++ yyevac/src/main/cpp/engine/render.cpp
@@ -115,14 +115,16 @@ void yyeva::Render::draw() {
         rgbArray->setVertexAttribPointer(aTextureRgbLocation);
         //启动混合
         glEnable(GL_BLEND);
+        glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
         //基于alpha通道的半透明混合函数
         //void glBlendFuncSeparate(GLenum srcRGB,
         //     GLenum dstRGB,
         //     GLenum srcAlpha,
         //     GLenum dstAlpha);
-        glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE,
-                            GL_ONE_MINUS_SRC_ALPHA);
+//        glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE,
+//                            GL_ONE_MINUS_SRC_ALPHA);
+
         glDisable(GL_BLEND);
     }
 }
